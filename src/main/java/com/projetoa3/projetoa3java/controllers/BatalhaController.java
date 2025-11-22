44791659ccd51e5f0d4e9472fb8480c70571b2e0
Gabriel Tassa <tassacesar@gmail.com>
2025-11-22 13:01:04 -0300

    :wastebasket: remove: removendo chamada e comentários desnecessários

--- src/main/java/com/projetoa3/projetoa3java/controllers/BatalhaController.java
+++ src/main/java/com/projetoa3/projetoa3java/controllers/BatalhaController.java
@@ -21,40 +21,6 @@ public class BatalhaController {
         this.batalhaService = batalhaService;
     }
 
-    @GetMapping("/iniciar")
-    public String iniciarBatalha() {
-        batalhaIniciada = true;
-        return "A batalha iniciou!";
-    }
-
-    // @PostMapping("/resultado")
-    // public Map<String, String> resultado(@RequestBody Map<String, Object> dados) {
-    //     if (!batalhaIniciada) {
-    //         return Map.of("erro", "A batalha não foi iniciada.");
-    //     }
-    //     String gladiador1 = (String) dados.get("gladiador1");
-    //     String gladiador2 = (String) dados.get("gladiador2");
-    //     String arma1 = (String) dados.get("arma1");
-    //     String arma2 = (String) dados.get("arma2");
-    //     boolean armadura1 = (boolean) dados.get("armadura1");
-    //     boolean armadura2 = (boolean) dados.get("armadura2");
-
-    //     String vencedor = batalhaService.iniciarBatalha(gladiador1, gladiador2, arma1, arma2, armadura1, armadura2);
-
-    //     batalhaIniciada = false;
-
-    //     resultadoBatalha = Map.of(
-    //         "gladiador1", gladiador1,
-    //         "gladiador2", gladiador2,
-    //         "arma1", arma1,
-    //         "arma2", arma2,
-    //         "armadura1", String.valueOf(armadura1),
-    //         "armadura2", String.valueOf(armadura2),
-    //         "vencedor", vencedor);
-
-    //     return resultadoBatalha;
-    // }
-
     @PostMapping("/batalhar")
     public ResultadoBatalhaResponse batalhar(@RequestBody IniciarBatalhaRequest req) {
         return batalhaService.iniciarBatalha(
--- src/main/java/com/projetoa3/projetoa3java/services/BatalhaService.java
+++ src/main/java/com/projetoa3/projetoa3java/services/BatalhaService.java
@@ -12,98 +12,6 @@ import com.projetoa3.projetoa3java.dto.ResultadoBatalhaResponse;
 
 @Service
 public class BatalhaService {
-    // private boolean batalhaAtiva = false;
-
-    // public String iniciarBatalha(String nome1, String nome2, String arma1, String arma2, boolean armadura1, boolean armadura2) {
-    //     batalhaAtiva = true;
-    //     Gladiador gladiador1 = new Gladiador(nome1, armadura1);
-    //     Gladiador gladiador2 = new Gladiador(nome2, armadura2);
-
-    //     Arma armaDoGladiador1 = new Arma(arma1);
-    //     Arma armaDoGladiador2 = new Arma(arma2);
-
-    //     Arena arena = new Arena(1000);
-
-    //     System.out.println("=== INÍCIO DO COMBATE ===\n");
-    //     gladiador1.exibirStatus();
-    //     gladiador2.exibirStatus();
-    //     arena.exibirStatusArena();
-
-    //     int rodada = 0;
-    //     boolean turnoGladiador1 = Math.random() < 0.5;
-
-    //     while (gladiador1.possuiVidas() && gladiador2.possuiVidas()) {
-    //         rodada++;
-    //         if (rodada > 9) {
-    //             return "Vocês lutaram bravamente, mas o combate terminou em empate após 10 rodadas.";
-    //         } // mudar lógica, funciona para lança e arco, porém se for espada, a batalha dura 8 rodadas.
-    //         System.out.println(">>> RODADA " + rodada + " <<<\n");
-
-    //         if (turnoGladiador1) {
-    //             System.out.println(gladiador1.getNomeGladiador() + " ataca " + gladiador2.getNomeGladiador()
-    //                     + " com " + armaDoGladiador1.getTipoArma());
-    //             gladiador2.receberGolpe(armaDoGladiador1);
-
-    //             if (gladiador2.possuiVidas()) {
-    //                 System.out.println(gladiador2.getNomeGladiador() + " revida com " + armaDoGladiador2.getTipoArma());
-    //                 gladiador1.receberGolpe(armaDoGladiador2);
-    //             } else {
-    //                 System.out.println(gladiador2.getNomeGladiador() + " foi morto e não conseguiu revidar.");
-    //             }
-    //         } else {
-    //             System.out.println(gladiador2.getNomeGladiador() + " ataca " + gladiador1.getNomeGladiador()
-    //                     + " com " + armaDoGladiador2.getTipoArma());
-    //             gladiador1.receberGolpe(armaDoGladiador2);
-
-    //             if (gladiador1.possuiVidas()) {
-    //                 System.out.println(gladiador1.getNomeGladiador() + " revida com " + armaDoGladiador1.getTipoArma());
-    //                 gladiador2.receberGolpe(armaDoGladiador1);
-    //             } else {
-    //                 System.out.println(gladiador1.getNomeGladiador() + " foi morto e não conseguiu revidar.");
-    //             }
-    //         }
-
-    //         turnoGladiador1 = !turnoGladiador1;
-
-    //         boolean g1Vivo = gladiador1.possuiVidas();
-    //         boolean g2Vivo = gladiador2.possuiVidas();
-
-    //         arena.atualizarHumorETorcida(gladiador1, gladiador2);
-
-    //         System.out.println("\n--- Resultado da rodada " + rodada + " ---");
-    //         gladiador1.exibirStatus();
-    //         gladiador2.exibirStatus();
-    //         arena.exibirStatusArena();
-
-    //         if (g1Vivo && g2Vivo) {
-    //             System.out.println("A batalha encerrou, vocês empataram, lutem novamente!");
-    //         } else if (g1Vivo && g2Vivo) {
-    //             System.out.println("Ambos os gladiadores continuam vivos!\n");
-    //         } else if (!g1Vivo && !g2Vivo) {
-    //             System.out.println("Ambos morreram! Empate trágico.\n");
-    //         } else if (!g1Vivo) {
-    //             System.out.println(
-    //                     gladiador1.getNomeGladiador() + " morreu. Vencedor: " + gladiador2.getNomeGladiador() + ".\n");
-    //         } else {
-    //             System.out.println(
-    //                     gladiador2.getNomeGladiador() + " morreu. Vencedor: " + gladiador1.getNomeGladiador() + ".\n");
-    //         }
-    //     }
-
-    //     System.out.println("=== FIM DO COMBATE ===\n");
-    //     System.out.println("Resumo final:");
-    //     gladiador1.exibirStatus();
-    //     gladiador2.exibirStatus();
-    //     arena.exibirStatusArena();
-
-    //     if (gladiador1.possuiVidas())
-    //         return gladiador1.getNomeGladiador();
-    //     else
-    //         return gladiador2.getNomeGladiador();
-    // }
-
-    //colocar numa lista
-
      public ResultadoBatalhaResponse iniciarBatalha(String nome1, String nome2,
                                                    String arma1, String arma2,
                                                    boolean armadura1, boolean armadura2) {
@@ -141,7 +49,7 @@ public class BatalhaService {
                 gladiador2.receberGolpe(armaDoGladiador1);
 
                 if (gladiador2.possuiVidas()) {
-                    System.out.println(gladiador2.getNomeGladiador() + " revida com " + armaDoGladiador2.getTipoArma());
+                    System.out.println(gladiador2.getNomeGladiador() + " revida' com " + armaDoGladiador2.getTipoArma());
                     gladiador1.receberGolpe(armaDoGladiador2);
                     acao1 = gladiador1.getNomeGladiador() + " ataca " + gladiador2.getNomeGladiador() + " com " + armaDoGladiador1.getTipoArma();
                     acao2 = gladiador2.getNomeGladiador() + " revida com " + armaDoGladiador2.getTipoArma();
